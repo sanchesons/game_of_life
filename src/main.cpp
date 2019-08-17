233b25f5409e8ad5991ac8c55b01b722ff767ef3
a.lagutin <sanche_88@mail.ru>
2019-08-17 15:29:50 +0300

    catch bad input data

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,41 @@
 #include "gol.h"
 #include "gol_view.h"
 #include <charconv>
+#include <optional>
 
-auto parse_cell(const std::string& str)
+std::optional<gol::Cell> parse_cell(const std::string& str)
 {
-    gol::Cell cell;
+    auto cell = std::make_optional<gol::Cell>();
     const auto* end = str.c_str()+str.size();
-    if (auto [next, error] = std::from_chars(str.c_str(), end, cell.x); error != std::errc()) {
-        throw std::runtime_error("Bad input");
-    } else if(auto error = std::from_chars(next+1, end, cell.y).ec; error != std::errc()) {
-        throw std::runtime_error("Bad input");
+    if (auto [next, error] = std::from_chars(str.c_str(), end, cell->x); error != std::errc()) {
+        return {};
+    } else if(auto error = std::from_chars(next+1, end, cell->y).ec; error != std::errc()) {
+        return {};
     }
     return cell;
 }
 
+std::optional<gol::Game::Citizens> read_citizens()
+{
+    auto cell_list = std::make_optional<gol::Game::Citizens>();
+    for (std::string line; std::getline(std::cin, line);) {
+        if (auto cell = parse_cell(line); cell) {
+            cell_list->emplace(*cell);
+        } else {
+            return {};
+        }
+    }
+    return cell_list;
+}
+
 int main(int argc, const char* args[])
 {
-    gol::Game::Citizens cell_list;
-    for (std::string line; std::getline(std::cin, line);) {
-        cell_list.emplace(parse_cell(line));
+    auto citizens = read_citizens();
+    if (!citizens) {
+        std::cout << "bad input data format" << std::endl;
+        return 1;
     }
-
-    auto game = gol::Game(std::move(cell_list));
+    auto game = gol::Game(std::move(*citizens));
     auto view = gol::GameView(game, {{0,0}, {80,20}});
     for(auto i = uint64_t(0); i < 60 && game.next() == gol::Game::NextStatus::OK; ++i) {
         view.print();
